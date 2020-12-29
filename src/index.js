03b0699e1488e00f8af165e0856f19f8c45995cf
Archit Gupta <rarchitgupta@gmail.com>
2020-12-29 13:13:44 +0530

    Code formatting + password hashing

--- src/index.js
+++ src/index.js
@@ -12,6 +12,21 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+const bcrypt = require("bcryptjs");
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+const myHashing = async () => {
+  const password = "Red12345!";
+  const hashedPass = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPass);
+
+  const isMatch = await bcrypt.compare("Red12345", hashedPass);
+  console.log(isMatch);
+};
+
+myHashing();
