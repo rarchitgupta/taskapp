82f31051f83ca67b7c1cf35343812b6e02951628
Archit Gupta <rarchitgupta@gmail.com>
2021-02-09 09:14:12 +0530

    Reconfigure hashing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
@@ -12,21 +10,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-const bcrypt = require("bcryptjs");
-
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const myHashing = async () => {
-  const password = "Red12345!";
-  const hashedPass = await bcrypt.hash(password, 8);
-
-  console.log(password);
-  console.log(hashedPass);
-
-  const isMatch = await bcrypt.compare("Red12345", hashedPass);
-  console.log(isMatch);
-};
-
-myHashing();
